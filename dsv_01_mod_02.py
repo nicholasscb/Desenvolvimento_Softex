49c866ec8b768ffe003c9271f5dce626770680a9
unknown <nicholascrescencio@gmail.com>
2022-07-25 20:34:40 -0300

    first commit

---
+++ dsv_01_mod_02.py
@@ -0,0 +1,9 @@
+nome = 'Nicholas'
+nota1 = 7.5
+nota2 = 6.5
+faltas = 4
+media = (nota1 + nota2)/2
+if (media >= 7.0) and (faltas <= 3):
+    print(nome, "foi aprovado!")
+else:
+    print(nome, "foi reprovado!")
